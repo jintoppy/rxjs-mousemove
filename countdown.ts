e832738a7dff0f4525d8eebb1b1f67989ba5b4fc
Jinto Jose <jintoppy@gmail.com>
2021-09-09 11:31:20 +0530

    adding query selectors

--- countdown.ts
+++ countdown.ts
@@ -1,1 +1,8 @@
 import './style.css';
+import { fromEvent } from 'rxjs';
+
+const startBtn = document.querySelector('#startBtn');
+const stopBtn = document.querySelector('#stopBtn');
+const counterDisplayHeader = document.querySelector('h3');
+
+
