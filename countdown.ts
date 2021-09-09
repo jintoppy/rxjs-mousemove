f0779d792ccc49528d35d31962e4c0fe7f7ad715
Jinto Jose <jintoppy@gmail.com>
2021-09-09 12:22:54 +0530

    countdown timer

--- countdown.ts
+++ countdown.ts
@@ -1,8 +1,42 @@
 import './style.css';
-import { fromEvent } from 'rxjs';
+import { fromEvent, interval, merge, empty } from 'rxjs';
+import { switchMapTo, take, takeWhile, takeUntil, switchMap, concatMap, scan, mapTo, startWith, tap } from 'rxjs/operators';
+import { EMPTY_OBSERVER } from 'rxjs/dist/types/internal/Subscriber';
+
 
 const startBtn = document.querySelector('#startBtn');
 const stopBtn = document.querySelector('#stopBtn');
+const pauseBtn = document.querySelector('#pauseBtn');
 const counterDisplayHeader = document.querySelector('h3');
+
+const startClick$ = fromEvent(startBtn, 'click');
+const stopClick$ = fromEvent(stopBtn, 'click');
+const pauseBtn$ = fromEvent(pauseBtn, 'click');
+const interval$ = interval(1000);
+
+const startValue = 5;
+
+merge(startClick$.pipe(mapTo(true)), pauseBtn$.pipe(mapTo(false))).pipe(
+    tap(val => {
+        console.log(val);
+    }),
+    switchMap((val) => val ? interval(1000) : empty()),
+    mapTo(-1),
+    scan((acc: number, curr: number) => {
+        return acc + curr;
+    }, startValue),
+    takeWhile(val => val >= 0),
+    takeUntil(stopClick$),
+    startWith(startValue)
+)
+.subscribe(val => {
+    console.log('inside subscribe');
+    counterDisplayHeader.innerHTML = val;
+}, 
+() => {},
+() => {
+    console.log('completed');
+})
+
 
 
