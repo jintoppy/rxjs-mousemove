f7cd1f075b9e4c39e9afd94dd6700b2f8d94e581
Jinto Jose <jintoppy@gmail.com>
2021-09-10 10:19:54 +0530

    polling finish

--- polling.ts
+++ polling.ts
@@ -1,5 +1,26 @@
-import './style.css';
+import { timer, fromEvent } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
+import { switchMap, mergeMap, takeUntil, tap, repeat } from 'rxjs/operators';
+import './style.css';
 
-ajax.get('https://picsum.photos/200/300')
+const startButton = document.querySelector('#start');
+const stopButton = document.querySelector('#stop');
+const resultEl = document.querySelector('#result');
+
+const start$ = fromEvent(startButton, 'click');
+const stop$ = fromEvent(stopButton, 'click');
+
+
+start$
+    .pipe(
+        switchMap(() => timer(0, 5000)),
+        switchMap(() => ajax.getJSON('https://api.thecatapi.com/v1/images/search')),
+        takeUntil(stop$),
+        repeat()
+    )
+    .subscribe(response => {        
+        resultEl.innerHTML = response[0].url;
+    })
+
+
 
